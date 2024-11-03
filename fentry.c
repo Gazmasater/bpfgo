b96c8b3a86f55ff7491a3704b964d6ba011ef568
Lev <tolkpplus@gmail.com>
2024-11-04 00:07:06 +0300

    inet_accept pid

--- fentry.c
+++ fentry.c
@@ -1,23 +1,38 @@
-#include "bpf/bpf_helpers.h"
-#include "bpf/bpf_core_read.h"
-#include "bpf/bpf.h"
-#include "netinet/in.h"
+#include "vmlinux.h"
+
+#include "bpf/bpf_tracing.h"
+#include  "bpf/bpf_endian.h"
 
 
 
-SEC("kprobe/do_accept")
-int bpf_prog(struct pt_regs *ctx, int sockfd, struct sockaddr __user *addr, int __user *addrlen) {
-    struct task_struct *task = (struct task_struct *)bpf_get_current_task();
+struct conn_info_t {
+    u32 pid;
+    u32 ip;
+    u16 port;
+};
+
+char LICENSE[] SEC("license") = "Dual BSD/GPL";
+
+
+SEC("kprobe/inet_accept")
+int trace_accept(struct pt_regs *ctx) {
+    struct sock *sk = (struct sock *)PT_REGS_PARM1(ctx);
+    u32 pid = bpf_get_current_pid_tgid() >> 32;
+
+    struct conn_info_t info = {};
+    info.pid = pid;
     
-    // Получаем информацию о задаче
-    char comm[TASK_COMM_LEN];
-    bpf_get_current_comm(&comm, sizeof(comm));
+    // Получаем IP-адрес и порт из структуры sock
+    u16 dport = sk->__sk_common.skc_dport;
+    u32 dip = sk->__sk_common.skc_daddr;
+    
+    info.ip = dip;
+    info.port = __bpf_ntohs(dport);
+        //bpf_printk("Connection accepted: PID=%d, IP=%x, PORT=%d\n", info.pid, info.ip, info.port);
+      bpf_printk("Connection accepted: PID=%d\n", info.pid);
+      //bpf_printk("Connection accepted: PID=%d, IP=%x\n", info.pid, info.ip);
 
-    // Здесь вы можете логировать информацию или делать что-то другое
-    bpf_trace_printk("Process %s accepted a connection on sockfd: %d\n", comm, sockfd);
+
 
     return 0;
 }
-
-char _license[] SEC("license") = "GPL";
-
