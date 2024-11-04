21a7f632fedc11070a174c4544e55efc023add37
Lev <tolkoplus@gmail.com>
2024-11-04 19:28:37 +0300

    измен карты на PERF_EVENT_ARRAY

--- fentry.c
+++ fentry.c
@@ -1,4 +1,4 @@
-//go:build ignore
+// go:build ignore
 
 #include "common.h"
 #include "bpf/bpf_endian.h"
@@ -44,10 +44,16 @@ struct sock
 	struct sock_common __sk_common;
 };
 
+// struct
+// {
+// 	__uint(type, BPF_MAP_TYPE_RINGBUF);
+// 	__uint(max_entries, 1 << 24);
+// } events SEC(".maps");
+
 struct
 {
-	__uint(type, BPF_MAP_TYPE_RINGBUF);
-	__uint(max_entries, 1 << 24);
+	__uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
+	__uint(max_entries, 8);
 } events SEC(".maps");
 
 struct event
@@ -63,35 +69,35 @@ struct event
 struct event *unused __attribute__((unused));
 
 SEC("fentry/inet_accept")
-int BPF_PROG(inet_accept, struct sock *sk){
-
+int BPF_PROG(inet_accept, struct sock *sk)
+{
 
 	struct event *tcp_info;
-	//struct event tcp_info = {};s
+	// struct event tcp_info = {};s
 
-
-	//if (sk->__sk_common.skc_family == AF_INET)
+	// if (sk->__sk_common.skc_family == AF_INET)
 	//{
-		tcp_info = bpf_ringbuf_reserve(&events, sizeof(struct event), 0);
-		if (!tcp_info)
-		{
-			return 0;
-		}
-		//tcp_info->saddr = sk->__sk_common.skc_rcv_saddr;
-		tcp_info->daddr = sk->__sk_common.skc_daddr;
-		tcp_info->dport = sk->__sk_common.skc_dport;
-		tcp_info->sport = bpf_htons(sk->__sk_common.skc_num);
+	tcp_info = bpf_ringbuf_reserve(&events, sizeof(struct event), 0);
+	if (!tcp_info)
+	{
+		return 0;
+	}
+	// tcp_info->saddr = sk->__sk_common.skc_rcv_saddr;
+	tcp_info->daddr = sk->__sk_common.skc_daddr;
+	tcp_info->dport = sk->__sk_common.skc_dport;
+	tcp_info->sport = bpf_htons(sk->__sk_common.skc_num);
 	//}
 	//  else if (sk->__sk_common.skc_family == AF_INET6)
 	//   {
-	
-	//   	return 0; 
+
+	//   	return 0;
 	//    }
 
 	bpf_get_current_comm(&tcp_info->comm, TASK_COMM_LEN);
 	tcp_info->pid = bpf_get_current_pid_tgid() >> 32; // Получаем PID
 
-	bpf_ringbuf_submit(tcp_info, 0);
+	// bpf_ringbuf_submit(tcp_info, 0);
+	bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU, &tcp_info, sizeof(tcp_info));
 
 	return 0;
 }
