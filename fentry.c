61052f486b32e5d1c3899d1f3c31e889ef07038f
Lev <tolkoplus@gmail.com>
2024-11-07 19:34:28 +0300

    add tcp

--- fentry.c
+++ fentry.c
@@ -1,3 +1,4 @@
+
 #include "vmlinux.h"
 #include "bpf/bpf_tracing.h"
 #include "bpf/bpf_endian.h"
@@ -35,10 +36,10 @@ int trace_accept(struct pt_regs *ctx) {
     u32 pid = bpf_get_current_pid_tgid() >> 32;
 
     struct conn_info_t info = {};
-    info.pid = pid;
+    info.pid = pid;  // Инициализация только один раз
     bpf_get_current_comm(&info.comm, sizeof(info.comm));
-    info.file = (struct file *)PT_REGS_PARM1(ctx);
 
+    // Обновляем карту с информацией о соединении
     int res = bpf_map_update_elem(&conn_info_map, &pid, &info, BPF_ANY);
     if (res == 0) {
         bpf_printk("Connection initiated: PID=%d, Comm=%s\n", pid, info.comm);
@@ -49,46 +50,78 @@ int trace_accept(struct pt_regs *ctx) {
     return 0;
 }
 
-
 SEC("kretprobe/__sys_accept4")
 int trace_accept_ret(struct pt_regs *ctx) {
+    struct sock *sk = (struct sock *)PT_REGS_PARM1(ctx);  // Получаем указатель на struct sock
     u32 pid = bpf_get_current_pid_tgid() >> 32;
-    int sockfd = PT_REGS_RC(ctx);
 
-    if (sockfd < 0) {
-        // Если прием соединения не удался, удаляем запись
-        bpf_map_delete_elem(&conn_info_map, &pid);
-        bpf_printk("Accept failed, removing connection info for PID=%d\n", pid);
-    } else {
-        // Соединение успешно принято, сохраняем дескриптор сокета
-        bpf_map_update_elem(&sockfd_map, &pid, &sockfd, BPF_ANY);
-        
-        // Получаем сохраненную информацию о процессе
-        struct conn_info_t *info = bpf_map_lookup_elem(&conn_info_map, &pid);
-        if (info) {
-            // Теперь получаем информацию о сокете через sockfd
-            struct sock *sk = (struct sock *)bpf_map_lookup_elem(&sockfd_map, &pid);
-            if (sk) {
-                // Извлекаем локальный и удаленный порты через skc_portpair
-                u16 local_port = 0;
-                u16 remote_port = 0;
+    bpf_printk("kretprobe PID=%d\n", pid);
 
-                // Получаем порты из skc_portpair
-                struct sk_buff *skb = (struct sk_buff *)sk;
-                if (skb) {
-                    struct skc_portpair *port_pair = &skb->skc_portpair;
-
-                    // Локальный порт
-                    local_port = bpf_ntohs(port_pair->local);
-                    // Удаленный порт
-                    remote_port = bpf_ntohs(port_pair->remote);
-
-                    bpf_printk("Accepted connection: PID=%d, Comm=%s, Local Port=%d, Remote Port=%d\n",
-                               pid, info->comm, local_port, remote_port);
-                }
-            }
-        }
+    // Получаем сохраненную информацию о процессе
+    struct conn_info_t *info = bpf_map_lookup_elem(&conn_info_map, &pid);
+    if (!info) {
+        bpf_printk("No conn_info found for PID=%d\n", pid);
+        return 0;
     }
+
+    u32 dip;
+    u32 skc_portpair;
+
+    // Чтение IP-адреса и порта из структуры sock
+    if (bpf_core_read(&dip, sizeof(dip), &sk->__sk_common.skc_rcv_saddr) != 0 ||
+        bpf_core_read(&skc_portpair, sizeof(skc_portpair), &sk->__sk_common.skc_portpair) != 0) {
+        return 0;
+    }
+
+    info->ip = dip;
+
+    // Извлечение локального и удаленного портов
+    info->sport = skc_portpair >> 16;  // Локальный порт (старшие 16 бит)
+    info->dport = skc_portpair & 0xFFFF;  // Удаленный порт (младшие 16 бит)
+
+    bpf_printk("Connection accepted: PID=%d, Sport=%u, Dport=%u\n", info->pid, info->sport, info->dport);
+
+    // Обновляем карту с информацией о соединении
+    bpf_map_update_elem(&conn_info_map, &pid, info, BPF_ANY);
+    bpf_printk("Connection started: PID=%d, Comm=%s\n", info->pid, info->comm);
+
+    return 0;
+}
+
+SEC("kprobe/tcp_v4_connect")
+int trace_tcp_connect(struct pt_regs *ctx) {
+    struct sock *sk = (struct sock *)PT_REGS_PARM1(ctx);
+    u32 pid = bpf_get_current_pid_tgid() >> 32;
+
+    struct conn_info_t conn_info = {};
+    conn_info.pid = pid;
+
+    // Чтение IP-адресов и портов из структуры sock
+    if (bpf_core_read(&conn_info.ip, sizeof(conn_info.ip), &sk->__sk_common.skc_rcv_saddr) != 0)
+    {
+        bpf_printk("!!!!!!!!!!!!!!!!!!!!!!TCP!!!!!!!!!!!!!!!!!!!!!!!!!");
+
+        return 0;
+    }
+    
+    u32 portpair;
+    if (bpf_core_read(&portpair, sizeof(portpair), &sk->__sk_common.skc_portpair) != 0)
+
+    {
+       
+        bpf_printk("!!!!!!!!!!!!!!!!!!!!!!PORTPAIR!!!!!!!!!!!!!!!!!!!!!!!!!");
+
+        return 0;
+    }
+
+    // Извлечение локального и удаленного портов
+    conn_info.sport = portpair >> 16;   // Локальный порт (старшие 16 бит)
+    conn_info.dport = portpair & 0xFFFF; // Удаленный порт (младшие 16 бит)
+
+    // Обновление карты с информацией о подключении
+    bpf_map_update_elem(&conn_info_map, &pid, &conn_info, BPF_ANY);
+    bpf_printk("TCP connect: PID=%d, IP=%x, Sport=%u, Dport=%u\n",
+               pid, conn_info.ip, conn_info.sport, conn_info.dport);
 
     return 0;
 }
