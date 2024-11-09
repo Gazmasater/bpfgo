22c6b5289d3d019e203a7be288b33f5e9dcb61aa
Lev <tolkoplus@gmail.com>
2024-11-09 16:09:18 +0300

    func client

--- fentry.c
+++ fentry.c
@@ -25,25 +25,24 @@ struct {
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
 #define AF_INET 2
 
+static __always_inline int init_conn_info(u32 pid, struct pt_regs *ctx, const char *func) {
+    struct conn_info_t conn_info = {};
+    conn_info.pid = pid;
+    bpf_get_current_comm(&conn_info.comm, sizeof(conn_info.comm));
+    conn_info.sock_addr = (struct sockaddr *)PT_REGS_PARM2(ctx);
+    conn_info.addrlen = PT_REGS_PARM3(ctx);
+    bpf_map_update_elem(&conn_info_map, &pid, &conn_info, BPF_ANY);
+    bpf_printk("%s  PID=%d, Comm=%s\n", func, pid, conn_info.comm);
+    return 0;
+}
+
+
 
 SEC("kprobe/__sys_accept4")
 int trace_accept4_entry(struct pt_regs *ctx) {
     u64 current_pid_tgid = bpf_get_current_pid_tgid();
     u32 pid = current_pid_tgid >> 32;
-
-    struct conn_info_t conn_info = {};
-    conn_info.pid = pid;
-    bpf_get_current_comm(&conn_info.comm, sizeof(conn_info.comm));
-
-    // Сохраняем указатель на sockaddr и длину
-    conn_info.sock_addr = (struct sockaddr *)PT_REGS_PARM2(ctx);
-    conn_info.addrlen = PT_REGS_PARM3(ctx);
-
-    // Сохраняем начальную информацию по PID
-    bpf_map_update_elem(&conn_info_map, &pid, &conn_info, BPF_ANY);
-    bpf_printk("CLIENT Accept4 entry: PID=%d, Comm=%s\n", pid, conn_info.comm);
-
-    return 0;
+    return init_conn_info(pid , ctx, "CLIENT Accept4 entry:");
 }
 
 // kretprobe для завершения извлечения информации о соединении
@@ -86,8 +85,6 @@ int trace_accept4_ret(struct pt_regs *ctx) {
 
 
 
-    // Удаляем запись из карты после завершения обработки
-   // bpf_map_delete_elem(&conn_info_map, &pid);
 
     return 0;
 }
@@ -98,20 +95,7 @@ SEC("kprobe/__sys_bind")
 int trace_bind_entry(struct pt_regs *ctx) {
     u64 current_pid_tgid = bpf_get_current_pid_tgid();
     u32 pid = current_pid_tgid >> 32;
-
-    struct conn_info_t conn_info = {};
-    conn_info.pid = pid;
-    bpf_get_current_comm(&conn_info.comm, sizeof(conn_info.comm));
-
-    // Сохраняем указатель на sockaddr и длину
-    conn_info.sock_addr = (struct sockaddr *)PT_REGS_PARM2(ctx);
-    conn_info.addrlen = PT_REGS_PARM3(ctx);
-
-    // Сохраняем начальную информацию по PID
-    bpf_map_update_elem(&conn_info_map, &pid, &conn_info, BPF_ANY);
-    bpf_printk("SERVER Accept4 entry: PID=%d, Comm=%s\n", pid, conn_info.comm);
-
-    return 0;
+    return init_conn_info(pid , ctx, "SERVER Accept4 entry:");
 }
 
 // kretprobe для завершения извлечения информации о соединении
