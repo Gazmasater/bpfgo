6712362398c08ba8226135025077003400cd59f8
Lev <tolkoplus@gmail.com>
2024-11-10 20:57:32 +0300

    abc

--- fentry.c
+++ fentry.c
@@ -20,20 +20,38 @@ struct {
     __uint(max_entries, 1024);
     __type(key, u32);
     __type(value, struct conn_info_t);
-} conn_info_map SEC(".maps");
+} conn_info_map_ab SEC(".maps");
+
+struct {
+    __uint(type, BPF_MAP_TYPE_PERCPU_HASH);
+    __uint(max_entries, 1024);
+    __type(key, u32);
+    __type(value, struct conn_info_t);
+} conn_info_map_c SEC(".maps");
 
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
 #define AF_INET 2
 
-static __always_inline int init_conn_info(u32 pid, struct pt_regs *ctx) {
+static __always_inline int init_conn_info_ab(u32 pid, struct pt_regs *ctx) {
     struct conn_info_t conn_info = {};
     conn_info.pid = pid;
     bpf_get_current_comm(&conn_info.comm, sizeof(conn_info.comm));
     conn_info.sock_addr = (struct sockaddr *)PT_REGS_PARM2(ctx);
     conn_info.addrlen = PT_REGS_PARM3(ctx);
-    bpf_map_update_elem(&conn_info_map, &pid, &conn_info, BPF_ANY);
+    bpf_map_update_elem(&conn_info_map_ab, &pid, &conn_info, BPF_ANY);
     return 0;
 }
+
+static __always_inline int init_conn_info_c(u32 pid, struct pt_regs *ctx) {
+    struct conn_info_t conn_info = {};
+    conn_info.pid = pid;
+    bpf_get_current_comm(&conn_info.comm, sizeof(conn_info.comm));
+    conn_info.sock_addr = (struct sockaddr *)PT_REGS_PARM2(ctx);
+    conn_info.addrlen = PT_REGS_PARM3(ctx);
+    bpf_map_update_elem(&conn_info_map_c, &pid, &conn_info, BPF_ANY);
+    return 0;
+}
+
 
 
 
@@ -41,9 +59,9 @@ SEC("kprobe/__sys_accept4")
 int trace_accept4_entry(struct pt_regs *ctx) {
     u64 current_pid_tgid = bpf_get_current_pid_tgid();
     u32 pid = current_pid_tgid >> 32;
-    init_conn_info(pid , ctx);
+    init_conn_info_ab(pid , ctx);
 
-    struct conn_info_t *conn_info = bpf_map_lookup_elem(&conn_info_map, &pid); 
+    struct conn_info_t *conn_info = bpf_map_lookup_elem(&conn_info_map_ab, &pid); 
     if (conn_info) 
     { bpf_printk("CLIENT accept4 entry: PID=%d, Comm=%s\n", pid, conn_info->comm); }
     
@@ -59,11 +77,11 @@ int trace_accept4_ret(struct pt_regs *ctx) {
     // Если результат отрицательный, значит произошла ошибка
     if (ret < 0) {
         bpf_printk("Accept4 failed for PID=%d\n", pid);
-        bpf_map_delete_elem(&conn_info_map, &pid);
+        bpf_map_delete_elem(&conn_info_map_ab, &pid);
         return 0;
     }
 
-    struct conn_info_t *conn_info = bpf_map_lookup_elem(&conn_info_map, &pid);
+    struct conn_info_t *conn_info = bpf_map_lookup_elem(&conn_info_map_ab, &pid);
     if (!conn_info) {
         bpf_printk("No connection info found for PID=%d\n", pid);
         return 0;
@@ -100,9 +118,9 @@ SEC("kprobe/__sys_bind")
 int trace_bind_entry(struct pt_regs *ctx) {
     u64 current_pid_tgid = bpf_get_current_pid_tgid();
     u32 pid = current_pid_tgid >> 32;
-    init_conn_info(pid , ctx);
+    init_conn_info_ab(pid , ctx);
 
-    struct conn_info_t *conn_info = bpf_map_lookup_elem(&conn_info_map, &pid); 
+    struct conn_info_t *conn_info = bpf_map_lookup_elem(&conn_info_map_ab, &pid); 
     if (conn_info) 
     { bpf_printk("SERVER Bind entry: PID=%d, Comm=%s\n", pid, conn_info->comm); }
 
@@ -118,11 +136,11 @@ int trace_bind_ret(struct pt_regs *ctx) {
     // Если результат отрицательный, значит произошла ошибка
     if (ret < 0) {
         bpf_printk("Accept4 failed for PID=%d\n", pid);
-        bpf_map_delete_elem(&conn_info_map, &pid);
+        bpf_map_delete_elem(&conn_info_map_ab, &pid);
         return 0;
     }
 
-    struct conn_info_t *conn_info = bpf_map_lookup_elem(&conn_info_map, &pid);
+    struct conn_info_t *conn_info = bpf_map_lookup_elem(&conn_info_map_ab, &pid);
     if (!conn_info) {
         bpf_printk("No connection info found for PID=%d\n", pid);
         return 0;
@@ -158,9 +176,9 @@ int trace_connect_entry(struct pt_regs *ctx) {
     u64 current_pid_tgid = bpf_get_current_pid_tgid();
     u32 pid = current_pid_tgid >> 32;
 
-    init_conn_info(pid, ctx);
+    init_conn_info_c(pid, ctx);
 
-    struct conn_info_t *conn_info = bpf_map_lookup_elem(&conn_info_map, &pid);
+    struct conn_info_t *conn_info = bpf_map_lookup_elem(&conn_info_map_c, &pid);
     if (conn_info) {
         bpf_printk("!!!!!!CLIENT Connect entry: PID=%d, Comm=%s\n", pid, conn_info->comm);
     }
@@ -177,11 +195,11 @@ int trace_connect_ret(struct pt_regs *ctx) {
     // Если результат отрицательный, значит произошла ошибка
     if (ret < 0) {
         bpf_printk("Connect failed for PID=%d\n", pid);
-        bpf_map_delete_elem(&conn_info_map, &pid);
+        bpf_map_delete_elem(&conn_info_map_c, &pid);
         return 0;
     }
 
-    struct conn_info_t *conn_info = bpf_map_lookup_elem(&conn_info_map, &pid);
+    struct conn_info_t *conn_info = bpf_map_lookup_elem(&conn_info_map_c, &pid);
     if (!conn_info) {
         bpf_printk("No connection info found for PID=%d\n", pid);
         return 0;
