e179bd0c4e87793a1df4321135f189c3c41f3df4
Lev <tolkoplus@gmail.com>
2024-12-12 21:50:56 +0300

    tmp

--- tracepoint.c
+++ tracepoint.c
@@ -3,6 +3,20 @@
 #include "bpf/bpf_endian.h"
 #include "bpf/bpf_core_read.h"
 
+struct sys_enter_open_args
+{
+        unsigned short common_type;      
+        unsigned char common_flags;      
+        unsigned char common_preempt_count;      
+        int common_pid;     
+        int __syscall_nr; 
+        int pad1;
+        const char * filename;   
+        int flags;       
+        umode_t mode;   
+
+};
+
 // Определение структуры данных, передаваемой в userspace
 struct event {
     u32 pid;
@@ -17,7 +31,7 @@ struct {
 
 
 SEC("tracepoint/syscalls/sys_enter_open")
-int tracepoint_handler(struct tracepoint__syscalls__sys_enter_open *ctx) {
+int tracepoint_handler(struct sys_enter_open_args *ctx) {
     char msg[] = "sys_open called";
     bpf_perf_event_output(ctx, &perf_event_array, BPF_F_CURRENT_CPU, &msg, sizeof(msg));
     return 0;
