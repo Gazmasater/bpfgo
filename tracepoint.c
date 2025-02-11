81b659ddc065d3dda911c23d9ee245fae4a74576
Lev <tolkoplus@gmail.com>
2025-02-12 00:56:01 +0300

    tmp

--- tracepoint.c
+++ tracepoint.c
@@ -101,8 +101,13 @@ int trace_sendto_enter(struct sys_enter_sendto_args *ctx) {
     conn_info.pid = pid;
     bpf_get_current_comm(&conn_info.comm, sizeof(conn_info.comm));
 
+    struct sockaddr addr;
+
+    bpf_probe_read(&addr, sizeof(addr), ctx->addr);
+    bpf_map_update_elem(&fd_map, &pid, &addr, BPF_ANY);
+
+
     bpf_map_update_elem(&conn_info_map_sc, &pid, &conn_info, BPF_ANY);
-    bpf_map_update_elem(&fd_map, &pid, &ctx->addr, BPF_ANY);
 
     struct conn_info_t *stored_info = bpf_map_lookup_elem(&conn_info_map_sc, &pid);
     if (stored_info) {
@@ -112,38 +117,38 @@ int trace_sendto_enter(struct sys_enter_sendto_args *ctx) {
     return 0;
 }
 
-// SEC("tracepoint/syscalls/sys_exit_sendto")
-// int trace_sendto_exit(struct sys_exit_sendto_args *ctx) {
-//     u32 pid = bpf_get_current_pid_tgid() >> 32;
-//     long ret = ctx->ret;
+SEC("tracepoint/syscalls/sys_exit_sendto")
+int trace_sendto_exit(struct sys_exit_sendto_args *ctx) {
+    u32 pid = bpf_get_current_pid_tgid() >> 32;
+    long ret = ctx->ret;
 
-//     if (ret < 0) {
-//         bpf_map_delete_elem(&conn_info_map_sc, &pid);
-//         bpf_map_delete_elem(&fd_map, &pid);
-//         return 0;
-//     }
+    if (ret < 0) {
+        bpf_map_delete_elem(&conn_info_map_sc, &pid);
+        bpf_map_delete_elem(&fd_map, &pid);
+        return 0;
+    }
 
-//     struct conn_info_t *conn_info = bpf_map_lookup_elem(&conn_info_map_sc, &pid);
-//     if (!conn_info) {
-//         bpf_printk("UDP sys_exit_sendto: No connection info found for PID=%d\n", pid);
-//         bpf_map_delete_elem(&conn_info_map_sc, &pid);
-//         bpf_map_delete_elem(&fd_map, &pid);
-//         return 0;
-//     }
+    struct conn_info_t *conn_info = bpf_map_lookup_elem(&conn_info_map_sc, &pid);
+    if (!conn_info) {
+        bpf_printk("UDP sys_exit_sendto: No connection info found for PID=%d\n", pid);
+        bpf_map_delete_elem(&conn_info_map_sc, &pid);
+        bpf_map_delete_elem(&fd_map, &pid);
+        return 0;
+    }
 
-//     struct sockaddr_in *addr = bpf_map_lookup_elem(&fd_map, &pid);
-//     if (addr && addr->sin_family == AF_INET) {
-//         conn_info->src_ip = bpf_ntohl(addr->sin_addr.s_addr);
-//         conn_info->sport = bpf_ntohs(addr->sin_port);
+    struct sockaddr_in *addr = bpf_map_lookup_elem(&fd_map, &pid);
+     if (addr && addr->sin_family == AF_INET) {
+         conn_info->src_ip = bpf_ntohl(addr->sin_addr.s_addr);
+    //     conn_info->sport = bpf_ntohs(addr->sin_port);
 
-//         bpf_printk("UDP sys_exit_sendto: Connection: PID=%d, Comm=%s, IP=%d.%d.%d.%d, Port=%d\n",
-//                    conn_info->pid, conn_info->comm,
-//                    (conn_info->src_ip >> 24) & 0xFF, (conn_info->src_ip >> 16) & 0xFF,
-//                    (conn_info->src_ip >> 8) & 0xFF, conn_info->src_ip & 0xFF, conn_info->sport);
-//     }
+    //     bpf_printk("UDP sys_exit_sendto: Connection: PID=%d, Comm=%s, IP=%d.%d.%d.%d, Port=%d\n",
+    //                conn_info->pid, conn_info->comm,
+    //                (conn_info->src_ip >> 24) & 0xFF, (conn_info->src_ip >> 16) & 0xFF,
+    //                (conn_info->src_ip >> 8) & 0xFF, conn_info->src_ip & 0xFF, conn_info->sport);
+    }
 
-//     bpf_map_update_elem(&conn_info_map_sc, &pid, conn_info, BPF_ANY);
-//     bpf_map_delete_elem(&fd_map, &pid);
-//     return 0;
-// }
+    // bpf_map_update_elem(&conn_info_map_sc, &pid, conn_info, BPF_ANY);
+    // bpf_map_delete_elem(&fd_map, &pid);
+    return 0;
+}
 
