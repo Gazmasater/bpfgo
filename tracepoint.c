bf9d6fd07b83b9df1f7f156561911e433410f765
Lev <tolkoplus@gmail.com>
2025-02-12 22:59:35 +0300

    tmp

--- tracepoint.c
+++ tracepoint.c
@@ -19,6 +19,33 @@ struct conn_info_t
     //      ^^^^^^ 16 мало. сделай 64 хотя бы
 };
 
+struct sys_enter_recvfrom_args {
+    unsigned short common_type;           
+    unsigned char common_flags;           
+    unsigned char common_preempt_count;   
+    int common_pid;                       
+    int __syscall_nr;                     
+    int pad1;                            
+    int fd;                               
+    void *ubuf;                           
+    size_t size;                          
+    unsigned int flags;                                              
+    struct sockaddr *addr;                
+    int *addr_len;                        
+} ;
+
+struct sys_exit_recvfrom_args {
+    unsigned short common_type;           
+    unsigned char common_flags;          
+    unsigned char common_preempt_count;  
+    int common_pid;                      
+    int __syscall_nr;                    
+    int pad1;                             
+    int ret;                             
+} ;
+
+
+
 struct
 {
     __uint(type, BPF_MAP_TYPE_HASH);
@@ -56,6 +83,7 @@ int trace_sendto_enter(struct trace_event_raw_sys_enter *ctx)
 {
     u32 pid = bpf_get_current_pid_tgid() >> 32;
     struct conn_info_t conn_info = {};
+    
 
     conn_info.pid = pid;
     bpf_get_current_comm(&conn_info.comm, sizeof(conn_info.comm));
@@ -113,5 +141,70 @@ int trace_sendto_exit(struct trace_event_raw_sys_exit *ctx)
     }
 
     // TODO вот тут должна быть отправка через perf_event_output conn_info в юзерспейс
+    return 0;
+}
+
+
+SEC("tracepoint/syscalls/sys_enter_recvfrom")
+int trace_recvfrom_enter(struct trace_event_raw_sys_enter *ctx) {
+    u32 pid = bpf_get_current_pid_tgid() >> 32;
+    struct conn_info_t conn_info = {};
+
+    conn_info.pid = pid;
+    bpf_get_current_comm(&conn_info.comm, sizeof(conn_info.comm));
+
+    struct sockaddr addr;
+    void *addr_ptr = (void *)ctx->args[4];
+
+    bpf_probe_read(&addr, sizeof(addr), addr_ptr);
+    bpf_map_update_elem(&fd_map, &pid, &addr, BPF_ANY);
+    bpf_map_update_elem(&conn_info_map_ra, &pid, &conn_info, BPF_ANY);
+
+    struct conn_info_t *stored_info = bpf_map_lookup_elem(&conn_info_map_ra, &pid);
+    if (stored_info) {
+        bpf_printk("CLIENT sys_enter_recvfrom: PID=%d, Comm=%s\n", stored_info->pid, stored_info->comm);
+    }
+
+    return 0;
+}
+
+SEC("tracepoint/syscalls/sys_exit_recvfrom")
+int trace_recvfrom_exit(struct trace_event_raw_sys_exit *ctx) {
+    u32 pid = bpf_get_current_pid_tgid() >> 32;
+    long ret = ctx->ret;
+
+    if (ret < 0) {
+        bpf_map_delete_elem(&conn_info_map_ra, &pid);
+        bpf_map_delete_elem(&fd_map, &pid);
+        return 0;
+    }
+
+    struct conn_info_t *conn_info = bpf_map_lookup_elem(&conn_info_map_ra, &pid);
+    if (!conn_info) {
+        bpf_printk("UDP sys_exit_recvfrom: No connection info found for PID=%d\n", pid);
+        bpf_map_delete_elem(&conn_info_map_ra, &pid);
+        bpf_map_delete_elem(&fd_map, &pid);
+        return 0;
+    }
+
+    struct sockaddr_in *addr = bpf_map_lookup_elem(&fd_map, &pid);
+    if (addr && addr->sin_family == AF_INET) {
+        __be32 ip_addr = 0;
+        __be16 port;
+
+        ip_addr = BPF_CORE_READ(addr, sin_addr.s_addr);
+        port = BPF_CORE_READ(addr, sin_port);
+
+        conn_info->dst_ip = bpf_ntohl(ip_addr);
+        conn_info->dport = bpf_ntohs(port);
+
+        bpf_printk("UDP sys_exit_recvfrom: Connection: PID=%d, Comm=%s, IP=%d.%d.%d.%d, Port=%d\n",
+                   conn_info->pid, conn_info->comm,
+                   (conn_info->dst_ip >> 24) & 0xFF, (conn_info->dst_ip >> 16) & 0xFF,
+                   (conn_info->dst_ip >> 8) & 0xFF, conn_info->dst_ip & 0xFF, conn_info->dport);
+    }
+
+    bpf_map_update_elem(&conn_info_map_ra, &pid, conn_info, BPF_ANY);
+    bpf_map_delete_elem(&fd_map, &pid);
     return 0;
 }
