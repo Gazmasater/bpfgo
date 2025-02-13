5887d08e97494527fb8f9a14cc7340e31299c125
Lev <tolkoplus@gmail.com>
2025-02-13 22:45:02 +0300

    tmp

--- tracepoint.c
+++ tracepoint.c
@@ -9,8 +9,7 @@
 
 struct conn_info_t
 {
-
-	struct sockaddr *sock_addr;
+    struct sockaddr *sock_addr;
     u32 pid;
     u32 src_ip;
     u32 dst_ip;
@@ -22,67 +21,66 @@ struct conn_info_t
 
 struct sys_enter_sendto_args
 {
-
-        unsigned short common_type;       
-        unsigned char common_flags;       
-        unsigned char common_preempt_count;      
-        int common_pid;   
-        int __syscall_nr; 
-        int fd;  
-		int pad1;
-        void * buff;     
-        size_t len;      
-        unsigned int flags;       
-        struct sockaddr * addr;   
-        int addr_len;   
-
+    unsigned short common_type;
+    unsigned char common_flags;
+    unsigned char common_preempt_count;
+    int common_pid;
+    int __syscall_nr;
+    int fd;
+    int pad1;
+    void *buff;
+    size_t len;
+    unsigned int flags;
+    struct sockaddr *addr;
+    int addr_len;
 };
 
 struct sys_exit_sendto_args
 {
-
-		unsigned short common_type;      
-        unsigned char common_flags;     
-        unsigned char common_preempt_count;   
-        int common_pid;   
-        int __syscall_nr; 
-		int pad1;
-        long ret; 
-
+    unsigned short common_type;
+    unsigned char common_flags;
+    unsigned char common_preempt_count;
+    int common_pid;
+    int __syscall_nr;
+    int pad1;
+    long ret;
 };
 
 struct sys_enter_recvfrom_args {
-    unsigned short common_type;           
-    unsigned char common_flags;           
-    unsigned char common_preempt_count;   
-    int common_pid;                       
-    int __syscall_nr;                     
-    int pad1;                            
-    int fd;                               
-    void *ubuf;                           
-    size_t size;                          
-    unsigned int flags;                                              
-    struct sockaddr *addr;                
-    int *addr_len;                        
-} ;
+    unsigned short common_type;
+    unsigned char common_flags;
+    unsigned char common_preempt_count;
+    int common_pid;
+    int __syscall_nr;
+    int pad1;
+    int fd;
+    void *ubuf;
+    size_t size;
+    unsigned int flags;
+    struct sockaddr *addr;
+    int *addr_len;
+};
 
 struct sys_exit_recvfrom_args {
-    unsigned short common_type;           
-    unsigned char common_flags;          
-    unsigned char common_preempt_count;  
-    int common_pid;                      
-    int __syscall_nr;                    
-    int pad1;                             
-    int ret;                             
-} ;
+    unsigned short common_type;
+    unsigned char common_flags;
+    unsigned char common_preempt_count;
+    int common_pid;
+    int __syscall_nr;
+    int pad1;
+    int ret;
+};
 
-struct
-{
+struct {
     __uint(type, BPF_MAP_TYPE_HASH);
     __uint(max_entries, 1024);
     __type(key, u32);
     __type(value, struct conn_info_t);
 } conn_info_map SEC(".maps");
+
+struct {
+    __uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
+} events SEC(".maps");
 
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
 #define AF_INET 2
@@ -102,7 +100,6 @@ int trace_sendto_enter(struct sys_enter_sendto_args *ctx)
     return 0;
 }
 
-
 SEC("tracepoint/syscalls/sys_exit_sendto")
 int trace_sendto_exit(struct sys_exit_sendto_args *ctx)
 {
@@ -118,30 +115,25 @@ int trace_sendto_exit(struct sys_exit_sendto_args *ctx)
     struct conn_info_t *conn_info = bpf_map_lookup_elem(&conn_info_map, &pid);
     if (!conn_info)
     {
-        bpf_printk("UDP sys_exit_sendto: No connection info found for PID=%d\n", pid);
         return 0;
     }
 
-
     struct sockaddr_in addr = {};
-    if (conn_info->sock_addr) { 
+    if (conn_info->sock_addr)
+    {
         bpf_probe_read(&addr, sizeof(addr), conn_info->sock_addr);
-        if (addr.sin_family == AF_INET) {
+        if (addr.sin_family == AF_INET)
+        {
             conn_info->src_ip = bpf_ntohl(addr.sin_addr.s_addr);
             conn_info->sport = bpf_ntohs(addr.sin_port);
         }
     }
 
-
-    bpf_printk("UDP sys_exit_sendto: PID=%d, Comm=%s, IP=%d.%d.%d.%d, Port=%d\n",
-               conn_info->pid, conn_info->comm,
-               (conn_info->src_ip >> 24) & 0xFF, (conn_info->src_ip >> 16) & 0xFF,
-               (conn_info->src_ip >> 8) & 0xFF, conn_info->src_ip & 0xFF, conn_info->sport);
+    //bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU, conn_info, sizeof(*conn_info));
 
     bpf_map_delete_elem(&conn_info_map, &pid);
     return 0;
 }
-
 
 SEC("tracepoint/syscalls/sys_enter_recvfrom")
 int trace_recvfrom_enter(struct sys_enter_recvfrom_args *ctx)
@@ -177,18 +169,17 @@ int trace_recvfrom_exit(struct sys_exit_recvfrom_args *ctx)
     }
 
     struct sockaddr_in addr = {};
-    if (conn_info->sock_addr) { 
+    if (conn_info->sock_addr)
+    {
         bpf_probe_read(&addr, sizeof(addr), conn_info->sock_addr);
-        if (addr.sin_family == AF_INET) {
+        if (addr.sin_family == AF_INET)
+        {
             conn_info->src_ip = bpf_ntohl(addr.sin_addr.s_addr);
             conn_info->sport = bpf_ntohs(addr.sin_port);
         }
     }
 
-    bpf_printk("UDP sys_exit_recvfrom: PID=%d, Comm=%s, Src_IP=%d.%d.%d.%d, Src_Port=%d\n",
-               conn_info->pid, conn_info->comm,
-               (conn_info->src_ip >> 24) & 0xFF, (conn_info->src_ip >> 16) & 0xFF,
-               (conn_info->src_ip >> 8) & 0xFF, conn_info->src_ip & 0xFF, conn_info->sport);
+    //bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU, conn_info, sizeof(*conn_info));
 
     bpf_map_delete_elem(&conn_info_map, &pid);
     return 0;
