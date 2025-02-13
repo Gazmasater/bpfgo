0810472758fc1323975484e2dfdc845a80c62372
Lev <tolkoplus@gmail.com>
2025-02-13 21:59:22 +0300

    tmp

--- tracepoint.c
+++ tracepoint.c
@@ -1,4 +1,4 @@
-// go:build ignore
+//go:build ignore
 
 #include "vmlinux.h"
 #include "bpf/bpf_tracing.h"
@@ -9,14 +9,46 @@
 
 struct conn_info_t
 {
+
+	struct sockaddr *sock_addr;
     u32 pid;
     u32 src_ip;
     u32 dst_ip;
     u32 addrlen;
     u16 sport;
     u16 dport;
-    char comm[16];
-    //      ^^^^^^ 16 мало. сделай 64 хотя бы
+    char comm[64];
+};
+
+struct sys_enter_sendto_args
+{
+
+        unsigned short common_type;       
+        unsigned char common_flags;       
+        unsigned char common_preempt_count;      
+        int common_pid;   
+        int __syscall_nr; 
+        int fd;  
+		int pad1;
+        void * buff;     
+        size_t len;      
+        unsigned int flags;       
+        struct sockaddr * addr;   
+        int addr_len;   
+
+};
+
+struct sys_exit_sendto_args
+{
+
+		unsigned short common_type;      
+        unsigned char common_flags;     
+        unsigned char common_preempt_count;   
+        int common_pid;   
+        int __syscall_nr; 
+		int pad1;
+        long ret; 
+
 };
 
 struct sys_enter_recvfrom_args {
@@ -44,167 +76,120 @@ struct sys_exit_recvfrom_args {
     int ret;                             
 } ;
 
-
-
 struct
 {
     __uint(type, BPF_MAP_TYPE_HASH);
     __uint(max_entries, 1024);
     __type(key, u32);
     __type(value, struct conn_info_t);
-} conn_info_map_sc SEC(".maps");
-//^^^^^^^^^^^^^^^ зачем такие сложные названия? можно же просто назвать conn_info_map
-
-struct
-{
-    __uint(type, BPF_MAP_TYPE_HASH);
-    __uint(max_entries, 1024);
-    __type(key, u32);
-    __type(value, struct conn_info_t);
-} conn_info_map_ra SEC(".maps");
-//^^^^^^^^^^^^^^^ эта мапа нигде не используется
-
-struct
-{
-    __uint(type, BPF_MAP_TYPE_HASH);
-    __uint(max_entries, 10240);
-    //                  ^^^^^^ 1024?
-    __type(key, u32);
-    __type(value, int);
-} fd_map SEC(".maps");
-//^^^^^^ название странное! по сути оно хранит sockaddr, ну так и назови типа sock_addr_map
+} conn_info_map SEC(".maps");
 
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
 #define AF_INET 2
 
-// TODO trace_event_raw_sys_enter и sys_enter_sendto выглядят странно. Лучше задать самописные структуры из описания точки и примеров cilium
 SEC("tracepoint/syscalls/sys_enter_sendto")
-int trace_sendto_enter(struct trace_event_raw_sys_enter *ctx)
+int trace_sendto_enter(struct sys_enter_sendto_args *ctx)
 {
     u32 pid = bpf_get_current_pid_tgid() >> 32;
     struct conn_info_t conn_info = {};
-    
 
     conn_info.pid = pid;
     bpf_get_current_comm(&conn_info.comm, sizeof(conn_info.comm));
 
-    struct sockaddr addr;
-    void *addr_ptr = (void *)ctx->args[4];
+    conn_info.sock_addr = ctx->addr;
 
-    bpf_probe_read(&addr, sizeof(addr), addr_ptr);
-    bpf_map_update_elem(&fd_map, &pid, &addr, BPF_ANY);
-    //  ^^^^^^^^^^^^^^^^^^^^^^^^^^^ Зачем использовать дополнительную мапу для хранения addr? можно ведь все хранить в одной conn_info_map_sc
-    bpf_map_update_elem(&conn_info_map_sc, &pid, &conn_info, BPF_ANY);
-
+    bpf_map_update_elem(&conn_info_map, &pid, &conn_info, BPF_ANY);
     return 0;
 }
 
+
 SEC("tracepoint/syscalls/sys_exit_sendto")
-int trace_sendto_exit(struct trace_event_raw_sys_exit *ctx)
+int trace_sendto_exit(struct sys_exit_sendto_args *ctx)
 {
     u32 pid = bpf_get_current_pid_tgid() >> 32;
     long ret = ctx->ret;
 
     if (ret < 0)
     {
-        bpf_map_delete_elem(&conn_info_map_sc, &pid);
-        bpf_map_delete_elem(&fd_map, &pid);
+        bpf_map_delete_elem(&conn_info_map, &pid);
         return 0;
     }
 
-    struct conn_info_t *conn_info = bpf_map_lookup_elem(&conn_info_map_sc, &pid);
+    struct conn_info_t *conn_info = bpf_map_lookup_elem(&conn_info_map, &pid);
     if (!conn_info)
     {
         bpf_printk("UDP sys_exit_sendto: No connection info found for PID=%d\n", pid);
         return 0;
     }
 
-    struct sockaddr_in *addr = bpf_map_lookup_elem(&fd_map, &pid);
-    //      ^^^^^^^^^^ в мапе же в sys_enter_sendto сохраняешь тип sockaddr, а тут используешь sockaddr_in
-    if (addr && addr->sin_family == AF_INET)
-    {
-        __be32 ip_addr = 0;
-        __be16 port;
 
-        ip_addr = BPF_CORE_READ(addr, sin_addr.s_addr); // TODO BPF_CORE_READ тут лишнее
-        port = BPF_CORE_READ(addr, sin_port);           // TODO BPF_CORE_READ тут лишнее
-
-        conn_info->src_ip = bpf_ntohl(ip_addr);
-        conn_info->sport = bpf_ntohs(port);
-
-        bpf_printk("UDP sys_exit_sendto: Connection: PID=%d, Comm=%s, IP=%d.%d.%d.%d, Port=%d\n",
-                   conn_info->pid, conn_info->comm,
-                   (conn_info->src_ip >> 24) & 0xFF, (conn_info->src_ip >> 16) & 0xFF,
-                   (conn_info->src_ip >> 8) & 0xFF, conn_info->src_ip & 0xFF, conn_info->sport);
-
-        bpf_map_update_elem(&conn_info_map_sc, &pid, conn_info, BPF_ANY);
+    struct sockaddr_in addr = {};
+    if (conn_info->sock_addr) { 
+        bpf_probe_read(&addr, sizeof(addr), conn_info->sock_addr);
+        if (addr.sin_family == AF_INET) {
+            conn_info->src_ip = bpf_ntohl(addr.sin_addr.s_addr);
+            conn_info->sport = bpf_ntohs(addr.sin_port);
+        }
     }
 
-    // TODO вот тут должна быть отправка через perf_event_output conn_info в юзерспейс
+
+    bpf_printk("UDP sys_exit_sendto: PID=%d, Comm=%s, IP=%d.%d.%d.%d, Port=%d\n",
+               conn_info->pid, conn_info->comm,
+               (conn_info->src_ip >> 24) & 0xFF, (conn_info->src_ip >> 16) & 0xFF,
+               (conn_info->src_ip >> 8) & 0xFF, conn_info->src_ip & 0xFF, conn_info->sport);
+
+    bpf_map_delete_elem(&conn_info_map, &pid);
     return 0;
 }
 
 
 SEC("tracepoint/syscalls/sys_enter_recvfrom")
-int trace_recvfrom_enter(struct trace_event_raw_sys_enter *ctx) {
+int trace_recvfrom_enter(struct sys_enter_recvfrom_args *ctx)
+{
     u32 pid = bpf_get_current_pid_tgid() >> 32;
     struct conn_info_t conn_info = {};
 
     conn_info.pid = pid;
     bpf_get_current_comm(&conn_info.comm, sizeof(conn_info.comm));
 
-    struct sockaddr addr;
-    void *addr_ptr = (void *)ctx->args[4];
+    conn_info.sock_addr = ctx->addr;
 
-    bpf_probe_read(&addr, sizeof(addr), addr_ptr);
-    bpf_map_update_elem(&fd_map, &pid, &addr, BPF_ANY);
-    bpf_map_update_elem(&conn_info_map_ra, &pid, &conn_info, BPF_ANY);
-
-    struct conn_info_t *stored_info = bpf_map_lookup_elem(&conn_info_map_ra, &pid);
-    if (stored_info) {
-        bpf_printk("CLIENT sys_enter_recvfrom: PID=%d, Comm=%s\n", stored_info->pid, stored_info->comm);
-    }
-
+    bpf_map_update_elem(&conn_info_map, &pid, &conn_info, BPF_ANY);
     return 0;
 }
 
 SEC("tracepoint/syscalls/sys_exit_recvfrom")
-int trace_recvfrom_exit(struct trace_event_raw_sys_exit *ctx) {
+int trace_recvfrom_exit(struct sys_exit_recvfrom_args *ctx)
+{
     u32 pid = bpf_get_current_pid_tgid() >> 32;
     long ret = ctx->ret;
 
-    if (ret < 0) {
-        bpf_map_delete_elem(&conn_info_map_ra, &pid);
-        bpf_map_delete_elem(&fd_map, &pid);
+    if (ret < 0)
+    {
+        bpf_map_delete_elem(&conn_info_map, &pid);
         return 0;
     }
 
-    struct conn_info_t *conn_info = bpf_map_lookup_elem(&conn_info_map_ra, &pid);
-    if (!conn_info) {
-        bpf_printk("UDP sys_exit_recvfrom: No connection info found for PID=%d\n", pid);
-        bpf_map_delete_elem(&conn_info_map_ra, &pid);
-        bpf_map_delete_elem(&fd_map, &pid);
+    struct conn_info_t *conn_info = bpf_map_lookup_elem(&conn_info_map, &pid);
+    if (!conn_info)
+    {
         return 0;
     }
 
-    struct sockaddr_in *addr = bpf_map_lookup_elem(&fd_map, &pid);
-    if (addr && addr->sin_family == AF_INET) {
-        __be32 ip_addr = 0;
-        __be16 port;
-
-        ip_addr = BPF_CORE_READ(addr, sin_addr.s_addr);
-        port = BPF_CORE_READ(addr, sin_port);
-
-        conn_info->dst_ip = bpf_ntohl(ip_addr);
-        conn_info->dport = bpf_ntohs(port);
-
-        bpf_printk("UDP sys_exit_recvfrom: Connection: PID=%d, Comm=%s, IP=%d.%d.%d.%d, Port=%d\n",
-                   conn_info->pid, conn_info->comm,
-                   (conn_info->dst_ip >> 24) & 0xFF, (conn_info->dst_ip >> 16) & 0xFF,
-                   (conn_info->dst_ip >> 8) & 0xFF, conn_info->dst_ip & 0xFF, conn_info->dport);
+    struct sockaddr_in addr = {};
+    if (conn_info->sock_addr) { 
+        bpf_probe_read(&addr, sizeof(addr), conn_info->sock_addr);
+        if (addr.sin_family == AF_INET) {
+            conn_info->src_ip = bpf_ntohl(addr.sin_addr.s_addr);
+            conn_info->sport = bpf_ntohs(addr.sin_port);
+        }
     }
 
-    bpf_map_update_elem(&conn_info_map_ra, &pid, conn_info, BPF_ANY);
-    bpf_map_delete_elem(&fd_map, &pid);
+    bpf_printk("UDP sys_exit_recvfrom: PID=%d, Comm=%s, Src_IP=%d.%d.%d.%d, Src_Port=%d\n",
+               conn_info->pid, conn_info->comm,
+               (conn_info->src_ip >> 24) & 0xFF, (conn_info->src_ip >> 16) & 0xFF,
+               (conn_info->src_ip >> 8) & 0xFF, conn_info->src_ip & 0xFF, conn_info->sport);
+
+    bpf_map_delete_elem(&conn_info_map, &pid);
     return 0;
 }
