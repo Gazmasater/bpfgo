8d56844629f57e4718e32baf23d1e1b2c5f0793c
Lev <tolkoplus@gmail.com>
2025-02-13 13:32:50 +0300

    tmp

--- tracepoint.c
+++ tracepoint.c
@@ -93,96 +93,62 @@ struct
     __type(value, struct conn_info_t);
 } conn_info_map_ra SEC(".maps");
 
-struct {
-    __uint(type, BPF_MAP_TYPE_HASH);
-    __uint(max_entries, 10240);
-    __type(key, u32);
-    __type(value, int);
-} fd_map SEC(".maps");
 
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
 #define AF_INET 2
-
-static __always_inline int init_conn_info(struct sockaddr *sock_addr, struct bpf_map_def *map, u32 pid, u8 call)
-{
-    struct conn_info_t conn_info = {};
-    conn_info.pid = pid;
-    bpf_get_current_comm(&conn_info.comm, sizeof(conn_info.comm));
-    conn_info.sock_addr = sock_addr;
-    bpf_map_update_elem(map, &pid, &conn_info, BPF_ANY);
-    return 0;
-}
-
-static __always_inline int init_conn_info_recvfrom(struct sys_enter_recvfrom_args *ctx)
-{
-    return init_conn_info((struct sockaddr *)ctx->addr, &conn_info_map_ra, bpf_get_current_pid_tgid() >> 32,3);
-}
 
 
 
 
 SEC("tracepoint/syscalls/sys_enter_sendto")
-int trace_sendto_enter(struct sys_enter_sendto_args *ctx) {
+int trace_sendto_enter(struct sys_enter_sendto_args *ctx)
+{
     u32 pid = bpf_get_current_pid_tgid() >> 32;
     struct conn_info_t conn_info = {};
-    
 
     conn_info.pid = pid;
     bpf_get_current_comm(&conn_info.comm, sizeof(conn_info.comm));
 
-    struct sockaddr_in addr;
-    void *addr_ptr = (void *)ctx->addr;
-    
+    struct sockaddr_in addr = {};
+    void *addr_ptr = (void *)ctx->addr;   
     bpf_probe_read(&addr, sizeof(addr), addr_ptr);
-    bpf_map_update_elem(&fd_map, &pid, &addr, BPF_ANY);
-    bpf_map_update_elem(&conn_info_map_sc, &pid, &conn_info, BPF_ANY);
 
-    struct conn_info_t *stored_info = bpf_map_lookup_elem(&conn_info_map_sc, &pid);
-    if (stored_info) {
-        bpf_printk("SERVER sys_enter_sendto: PID=%d, Comm=%s\n", stored_info->pid, stored_info->comm);
+    if (addr.sin_family == AF_INET)
+    {
+        conn_info.src_ip = bpf_ntohl(addr.sin_addr.s_addr);
+        conn_info.sport = bpf_ntohs(addr.sin_port);
     }
 
+    bpf_map_update_elem(&conn_info_map_sc, &pid, &conn_info, BPF_ANY);
     return 0;
 }
 
+
 SEC("tracepoint/syscalls/sys_exit_sendto")
-int trace_sendto_exit(struct sys_exit_sendto_args *ctx) {
+int trace_sendto_exit(struct sys_exit_sendto_args *ctx)
+{
     u32 pid = bpf_get_current_pid_tgid() >> 32;
     long ret = ctx->ret;
 
-    if (ret < 0) {
+    if (ret < 0)
+    {
         bpf_map_delete_elem(&conn_info_map_sc, &pid);
-        bpf_map_delete_elem(&fd_map, &pid);
         return 0;
     }
 
     struct conn_info_t *conn_info = bpf_map_lookup_elem(&conn_info_map_sc, &pid);
-    if (!conn_info) {
+    if (!conn_info)
+    {
         bpf_printk("UDP sys_exit_sendto: No connection info found for PID=%d\n", pid);
-        bpf_map_delete_elem(&conn_info_map_sc, &pid);
-        bpf_map_delete_elem(&fd_map, &pid);
         return 0;
     }
 
-    struct sockaddr_in *addr = bpf_map_lookup_elem(&fd_map, &pid);
-    if (addr && addr->sin_family == AF_INET) {
-        __be32 ip_addr = 0;
-        __be16 port;
+    bpf_printk("UDP sys_exit_sendto: PID=%d, Comm=%s, IP=%d.%d.%d.%d, Port=%d\n",
+               conn_info->pid, conn_info->comm,
+               (conn_info->src_ip >> 24) & 0xFF, (conn_info->src_ip >> 16) & 0xFF,
+               (conn_info->src_ip >> 8) & 0xFF, conn_info->src_ip & 0xFF, conn_info->sport);
 
-        ip_addr = addr->sin_addr.s_addr;
-        port = addr->sin_port;
-        conn_info->src_ip = bpf_ntohl(ip_addr);
-        conn_info->sport = bpf_ntohs(port);
-
-        bpf_printk("UDP sys_exit_sendto: Connection: PID=%d, Comm=%s, IP=%d.%d.%d.%d, Port=%d\n",
-                   conn_info->pid, conn_info->comm,
-                   (conn_info->src_ip >> 24) & 0xFF, (conn_info->src_ip >> 16) & 0xFF,
-                   (conn_info->src_ip >> 8) & 0xFF, conn_info->src_ip & 0xFF, conn_info->sport);
-    }
-
-    bpf_map_update_elem(&conn_info_map_sc, &pid, conn_info, BPF_ANY);
-    bpf_map_delete_elem(&fd_map, &pid);
+    // Можно удалить, если данные не нужны дальше
+    bpf_map_delete_elem(&conn_info_map_sc, &pid);
     return 0;
 }
-
-
