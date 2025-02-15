a415ca65bf0bf2c4a8ed30a80f101c8f8211c941
Lev <tolkoplus@gmail.com>
2025-02-15 06:36:05 +0300

    tmp

---
+++ Perf/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/dropbox/goebpf"
+)
+
+func main() {
+	// Инициализация системы eBPF
+	bpf := goebpf.NewDefaultEbpfSystem()
+
+	// Получение карты perf по имени
+	perfMap := bpf.GetMapByName("events")
+	if perfMap == nil {
+		log.Fatal("Не удалось найти карту perf с именем 'events'")
+	}
+
+	// Создание perf-событий
+	perfEvents, err := goebpf.NewPerfEvents(perfMap)
+	if err != nil {
+		log.Fatalf("Ошибка создания perf-событий: %v", err)
+	}
+	defer perfEvents.Stop()
+
+	// Запуск чтения событий
+	events, err := perfEvents.StartForAllProcessesAndCPUs(4096)
+	if err != nil {
+		log.Fatalf("Ошибка запуска чтения событий: %v", err)
+	}
+	defer perfEvents.Stop()
+
+	// Создание канала для обработки сигналов
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+	defer signal.Stop(sigCh)
+
+	// Обработка событий
+	for {
+		select {
+		case data := <-events:
+			// Преобразование данных в структуру event_t
+			var event struct {
+				Pid   uint32
+				Comm  [64]byte
+				SrcIP uint32
+				Sport uint16
+			}
+			err := binary.Read(bytes.NewReader(data), binary.LittleEndian, &event)
+			if err != nil {
+				log.Printf("Ошибка распаковки данных события: %v", err)
+				continue
+			}
+
+			// Вывод информации
+			fmt.Printf("PID: %d, Comm: %s, SrcIP: %d.%d.%d.%d, SrcPort: %d\n",
+				event.Pid,
+				string(event.Comm[:]),
+				(event.SrcIP>>24)&0xFF, (event.SrcIP>>16)&0xFF,
+				(event.SrcIP>>8)&0xFF, event.SrcIP&0xFF,
+				event.Sport)
+		case <-sigCh:
+			// Обработка сигнала завершения
+			fmt.Println("\nПолучен сигнал завершения. Завершение работы...")
+			return
+		}
+	}
+}
