f751c73773f9f57606ce3586e757264311f43aa9
Lev <tolkoplus@gmail.com>
2025-02-19 23:36:38 +0300

    tmp

--- main.go
+++ main.go
@@ -4,50 +4,40 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/cilium/ebpf"
 	"github.com/cilium/ebpf/link"
 	"github.com/cilium/ebpf/rlimit"
-	"github.com/pkg/errors"
 )
 
-var objs = target_amd64_bpfObjects{}
-
-func init() {
-	var loadOpts = &ebpf.CollectionOptions{}
-
-	// Снимаем ограничение на память
-	if err := rlimit.RemoveMemlock(); err != nil {
-		panic(errors.WithMessage(err, "failed to remove memory limit for process"))
-	}
-
-	// Инициализируем объекты eBPF
-
-	if err := loadTarget_amd64_bpfObjects(&objs, loadOpts); err != nil {
-		panic(errors.WithMessage(err, "failed to load bpf objects"))
-	}
-
-}
+var objs target_amd64_bpfObjects
 
 func main() {
+	// Снимаем ограничение на память
+	if err := rlimit.RemoveMemlock(); err != nil {
+		log.Fatalf("failed to remove memory limit for process: %v", err)
+	}
 
+	// Загружаем eBPF-объекты
+	if err := loadTarget_amd64_bpfObjects(&objs, nil); err != nil {
+		log.Fatalf("failed to load bpf objects: %v", err)
+	}
+	defer objs.Close()
+
+	// Привязываем eBPF-программу к tracepoint
 	kpEnter, err := link.Tracepoint("syscalls", "sys_enter_sendto", objs.TraceSendtoEnter, nil)
-
 	if err != nil {
 		log.Fatalf("opening tracepoint sys_enter_sendto: %s", err)
 	}
 	defer kpEnter.Close()
 
-	fmt.Println(" kpEnter", kpEnter)
+	fmt.Println("kpEnter:", kpEnter)
 
 	kpExit, err := link.Tracepoint("syscalls", "sys_exit_sendto", objs.TraceSendtoExit, nil)
 	if err != nil {
-		log.Fatalf("opening tracepoint sys_enter_sendto: %s", err)
+		log.Fatalf("opening tracepoint sys_exit_sendto: %s", err)
 	}
 	defer kpExit.Close()
 
-	fmt.Println(" kpExit", kpExit)
+	fmt.Println("kpExit:", kpExit)
 
-	// Печатаем сообщение о привязке
-	fmt.Println(" !!!!!")
-
+	// Оставляем программу запущенной, чтобы eBPF-программы продолжали работать
 }
