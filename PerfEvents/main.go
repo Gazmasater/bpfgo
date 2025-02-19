e50a1ac98f49a80b75a3cc3d60397e9e70ad7091
Lev <tolkoplus@gmail.com>
2025-02-19 18:59:33 +0300

    tmp

--- PerfEvents/main.go
+++ PerfEvents/main.go
@@ -1,44 +1,40 @@
 package main
 
 import (
+	gener "bpfgo/generated"
 	"fmt"
-	"log"
-	"os"
-	"path/filepath"
 
 	"github.com/cilium/ebpf"
+	"github.com/cilium/ebpf/rlimit"
+	"github.com/pkg/errors"
 )
 
-type TraceInfo struct {
-	Pid   uint32
-	SrcIP uint32
-	Sport uint16
-	Dport uint16
-	Comm  [16]byte
+type bpfObjects struct {
+	Programs map[string]*ebpf.Program
+	Maps     map[string]*ebpf.Map
 }
 
-func main() {
-	// Получаем текущую рабочую директорию
-	wd, err := os.Getwd()
-	if err != nil {
-		log.Fatalf("failed to get current working directory: %v", err)
+func init() {
+	// Снимаем ограничение на память
+	if err := rlimit.RemoveMemlock(); err != nil {
+		panic(errors.WithMessage(err, "failed to remove memory limit for process"))
 	}
 
-	// Путь к скомпилированному eBPF объекту
-	eBpfFilePath := filepath.Join(filepath.Dir(wd), "generated", "bpf_x86_bpfel.o")
-	fmt.Println("Loading eBPF object...")
-
-	// Загружаем eBPF коллекцию
-	objs, err := ebpf.LoadCollection(eBpfFilePath)
-	if err != nil {
-		log.Fatalf("failed to load eBPF collection: %v", err)
+	// Инициализируем объекты eBPF
+	objs := bpfObjects{}
+	if err := gener.LoadBpfObjects(&objs, loadOpts); err != nil {
+		panic(errors.WithMessage(err, "failed to load bpf objects"))
 	}
-	defer objs.Close()
 
-	// Печатаем имена всех программ в коллекции для проверки
+	// Печатаем все программы из коллекции
 	fmt.Println("Loaded eBPF collection programs:")
 	for name, program := range objs.Programs {
 		fmt.Printf("Program: %s, Type: %v\n", name, program.Type())
 	}
+}
 
+func main() {
+	// Основная логика программы
+	// init() уже выполнена, и eBPF объекты были загружены и напечатаны.
+	fmt.Println("Main function started.")
 }
