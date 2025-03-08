05a098387f7658c971d2c4cd6db471263fddbb74
Lev <tolkoplus@gmail.com>
2025-03-08 21:29:59 +0300

    tmp

--- netlink/netlink.go
+++ netlink/netlink.go
@@ -1,99 +1,105 @@
 package main
 
 import (
-	"bytes"
-	"encoding/binary"
 	"fmt"
 	"log"
 	"net"
-
-	"github.com/mdlayher/netlink"
-	"golang.org/x/sys/unix"
+	"syscall"
+	"unsafe"
 )
 
-// inetDiagMsg структура для декодирования ответа
-type inetDiagMsg struct {
-	Family  uint8
-	State   uint8
-	Timer   uint8
-	Retrans uint8
-	ID      inetDiagSockID
+const (
+	// NETLINK_INET_DIAG = 4
+	NETLINK_INET_DIAG = 4
+	// Протокол TCP
+	IPPROTO_TCP = 6
+)
+
+type InetDiagReqV2 struct {
+	Family   uint8
+	Protocol uint8
+	Pad      uint16
+	Scope    uint32
+	Filter   uint32
+	Flags    uint32
+	Timeout  uint32
+	RecvQ    uint32
+	SendQ    uint32
+	IDs      [4]uint32
 }
 
-// inetDiagSockID содержит информацию о соединении
-type inetDiagSockID struct {
-	SPort  uint16
-	DPort  uint16
-	Src    [4]byte // IPv4
-	Dst    [4]byte // IPv4
-	Iface  uint32
-	Cookie [8]uint8
+type InetDiagMsg struct {
+	State    uint8
+	Pad      uint8
+	Flags    uint16
+	Family   uint8
+	Protocol uint8
+	AddrLen  uint16
+	Port     uint16
+	DstPort  uint16
+}
+
+func getNetlinkSocket() (int, error) {
+	fd, err := syscall.Socket(syscall.AF_NETLINK, syscall.SOCK_RAW, syscall.NETLINK_INET_DIAG)
+	if err != nil {
+		return -1, fmt.Errorf("failed to create netlink socket: %v", err)
+	}
+	return fd, nil
+}
+
+func getLocalConnections() ([]InetDiagMsg, error) {
+	fd, err := getNetlinkSocket()
+	if err != nil {
+		return nil, err
+	}
+	defer syscall.Close(fd)
+
+	fmt.Printf("FD=%d\n", fd)
+
+	// Создание запроса
+	req := InetDiagReqV2{
+		Family:   syscall.AF_INET,
+		Protocol: IPPROTO_TCP,
+	}
+
+	fmt.Println("Family  Protocol", req.Family, req.Protocol)
+
+	// Отправка запроса
+	err = syscall.Sendto(fd, (*[unsafe.Sizeof(req)]byte)(unsafe.Pointer(&req))[:], 0, &syscall.SockaddrNetlink{Family: syscall.AF_NETLINK})
+	if err != nil {
+		return nil, fmt.Errorf("failed to send netlink message: %v", err)
+	}
+
+	fmt.Println("Otpravleno")
+
+	// Чтение ответа
+	buf := make([]byte, 128000)
+	n, _, err := syscall.Recvfrom(fd, buf, 0)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read netlink response: %v", err)
+	}
+
+	fmt.Println("Prochitano")
+
+	// Преобразуем данные ответа в структуру
+	var connections []InetDiagMsg
+	for i := 0; i < n; i += int(unsafe.Sizeof(InetDiagMsg{})) {
+		conn := *(*InetDiagMsg)(unsafe.Pointer(&buf[i]))
+		connections = append(connections, conn)
+	}
+
+	return connections, nil
 }
 
 func main() {
-	// Открытие соединения с netlink
-	conn, err := netlink.Dial(unix.NETLINK_INET_DIAG, nil)
+	pid := 5743 // Пример PID
+	conns, err := getLocalConnections()
 	if err != nil {
-		log.Fatalf("Ошибка подключения к netlink: %v", err)
-	}
-	defer conn.Close()
-
-	// PID процесса, для которого ищем соединение
-	const pid = 5198
-
-	// Формируем запрос для получения всех соединений (TCP)
-	req := netlink.Message{
-		Header: netlink.Header{
-			Type:  netlink.HeaderType(unix.NETLINK_INET_DIAG), // Прямой запрос на получение соединений
-			Flags: netlink.Request | netlink.Dump,
-		},
-		// Пример запроса для получения всех соединений (IPv4, TCP)
-		Data: []byte{
-			2,                // AF_INET (IPv4)
-			unix.IPPROTO_TCP, // Протокол TCP
-			0, 0, 0, 0, 0, 0, // Пустые данные
-		},
+		log.Fatalf("Error: %v", err)
 	}
 
-	fmt.Println("Отправляем запрос в netlink...")
-	fmt.Println("DATA", req.Data)
-
-	// Отправляем запрос
-	_, err = conn.Send(req)
-	if err != nil {
-		log.Fatalf("Ошибка отправки запроса: %v", err)
-	}
-
-	fmt.Println("Запрос отправлен, ожидаем ответ...")
-
-	// Читаем ответ
-	msgs, err := conn.Receive()
-	if err != nil {
-		log.Fatalf("Ошибка чтения ответа: %v", err)
-	}
-
-	fmt.Println("Ответ получен!")
-
-	// Разбираем ответ
-	for _, m := range msgs {
-		if len(m.Data) < 20 {
-			continue
-		}
-
-		var diag inetDiagMsg
-		reader := bytes.NewReader(m.Data)
-		err := binary.Read(reader, binary.LittleEndian, &diag)
-		if err != nil {
-			log.Printf("Ошибка декодирования сообщения: %v", err)
-			continue
-		}
-
-		// Преобразуем данные в читаемый формат
-		srcIP := net.IP(diag.ID.Src[:])
-		srcPort := binary.LittleEndian.Uint16(m.Data[8:10])
-
-		// Выводим информацию о процессе и соединении
-		fmt.Printf("Процесс %d использует IP: %s, Port: %d\n", pid, srcIP, srcPort)
-		fmt.Printf("Диагностика соединения: %+v\n", diag)
+	for _, conn := range conns {
+		ip := net.IPv4(byte(conn.AddrLen), byte(conn.DstPort), byte(conn.Port), byte(conn.State))
+		fmt.Printf("PID=%d Local IP: %s Port: %d\n", pid, ip, conn.Port)
 	}
 }
