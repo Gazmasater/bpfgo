ec028e63025577121798b0890bf09c95995445c6
Lev <tolkoplus@gmail.com>
2025-05-17 15:45:24 +0300

    struct trace new

--- trace.c
+++ trace.c
@@ -67,22 +67,15 @@ struct trace_info {
     struct sockaddr_in ddstIP; 
     struct in_addr srcIP;
     struct in_addr dstIP;
-
     __u32 srcIP6[4];    
     __u32 dstIP6[4];   
-    //  __u16 ssrcIP6[8];
-    // __u16 ddstIP6[8];
-
-    __u16 sport;
+       __u16 sport;
     __u16 dport;
-
     __u32 pid;
     __u32 proto;
     __u32 sysexit;
     __u32 state;
-
     __u16 family;
-
     char comm[32];
 } ;
 
